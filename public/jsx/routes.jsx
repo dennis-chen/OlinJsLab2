05f967e5e90de6aa060c2aa1888c8680908f2db4
David Zhu <huanzhendavidzhu@gmail.com>
2015-03-05 20:39:24 -0500

    Now with routes.

---
+++ public/jsx/routes.jsx
@@ -0,0 +1,41 @@
+var Router = ReactRouter;
+
+var DefaultRoute = Router.DefaultRoute;
+var Link = Router.Link;
+var Route = Router.Route;
+var RouteHandler = Router.RouteHandler;
+var NotFoundRoute = Router.NotFoundRoute;
+
+var App = React.createClass({
+  render: function () {
+    return (
+      <div>
+        <RouteHandler/>
+      </div>
+    );
+  }
+});
+
+// Test route to prove that routing works.
+// FIXME Remove this for production.
+var Test = React.createClass({
+  render: function () {
+    return (
+      <div>
+      	Hello world!
+      </div>
+    );
+  }
+});
+
+var routes = (
+  <Route name="app" path="/" handler={App}>
+    <Route name="test" handler={Test}/>
+    <NotFoundRoute handler={Room}/>
+    <DefaultRoute handler={Room}/>
+  </Route>
+);
+
+Router.run(routes, function (Handler) {
+  React.render(<Handler/>, document.getElementById('container'));
+});
