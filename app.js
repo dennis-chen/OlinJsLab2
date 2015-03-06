ddc3ed53f3a7f1554d2157565d9a00a574460d29
David Zhu <huanzhendavidzhu@gmail.com>
2015-03-05 19:03:23 -0500

    Created basic boilerplate.

---
+++ app.js
@@ -0,0 +1,43 @@
+// Utility Imports
+var express = require('express');
+var session = require('express-session');
+var mongoose = require('mongoose');
+var path = require('path');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+React = require('react');
+
+// Route Imports
+var base = require('./routes/base');
+
+// Environment Variables
+var mongoURI = process.env.MONGOURI || "mongodb://localhost/test";
+var PORT = process.env.PORT || 3000;
+mongoose.connect(mongoURI);
+
+// Config
+var app = express();
+
+// Middleware
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jsx');
+app.engine('jsx', require('express-react-views').createEngine());
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
+app.use(session({
+    secret: 'secret',
+    resave: false,
+    saveUninitialized: true
+}));
+
+// Routing Table
+
+//GET
+app.get('/', base.index);
+
+app.listen(PORT);
---
+++ views/index.jsx
@@ -0,0 +1,21 @@
+var MainLayout = React.createClass({render: function() {return (
+<html>
+<head>
+    <title>Something</title>
+    <link rel="stylesheet" type="text/css" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css"></link>
+    <link rel="stylesheet" type="text/css" href="stylesheets/base.css"></link>
+</head>
+
+<body>
+	Hello world!    
+</body>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/react/0.12.2/react.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/react/0.12.2/JSXTransformer.js"></script>
+
+</html>
+);}});
+
+module.exports = MainLayout;
