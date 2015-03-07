4223737fb0962a497674afd4b081b2e40f9ab500
David Zhu <huanzhendavidzhu@gmail.com>
2015-03-07 17:10:22 -0500

    Working with Synchronize.

--- routes/base.js
+++ routes/base.js
@@ -1,9 +1,14 @@
-function find_or_create_room (req, res) {
+var sync = require('synchronize');
+var fiber = sync.fiber;
+var await = sync.await;
+var defer = sync.defer;
+
+function find_or_create_room (req, res, modifyPlaylist) {
 	
 	var chosenPlaylist;
 	Playlist.find({name: req.body.roomId})
 		.exec(function (err, playlist){
-			if (err) console.error(err);
+			if (err) throw err;
 			
 			// Use existing playlist or make a new one.	
 			if (playlist.length != 0) {
@@ -18,15 +23,24 @@ function find_or_create_room (req, res) {
 			var upsertData = chosenPlaylist.toObject();
 			delete upsertData._id;
 
-			Playlist.update(
-				{_id: chosenPlaylist.id}, 
-				upsertData, 
-				{upsert: true}, 
-				function (err, result){
-					if (err) console.error(err);
+			try {
+		    fiber(function() {
+		    		if (modifyPlaylist != null) {
+		        	upsertData = await(modifyPlaylist(upsertData, defer()));
+		    		}
+		    		result = await(
+		    			Playlist.update(
+		    				{_id: chosenPlaylist.id}, 
+		    				upsertData, 
+		    				{upsert: true}, 
+		    				function (err, result){
+		    					if (err) throw err;
 
-					res.status(200).json({status: "success"});
-				});
+		    					res.status(200).json({status: "success"});
+		    				}), defer()
+		    		);
+		    });
+			} catch(err) {throw err};
 	});
 }
 
@@ -39,7 +53,11 @@ exports.add_song = function(req, res) {
 	// 	nextAction(chosenPlaylist, upsertData);
 	// });
 
-	find_or_create_room(req, res);
+	find_or_create_room(req, res, function (upsertData){
+		var newSong = new Song(req.body.song);
+		upsertData.songs.push(newSong);
+		console.log(upsertData);
+	});
 };
 
 exports.find_or_create_room = function(req, res) {
